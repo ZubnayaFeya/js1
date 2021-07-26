a8e6e420c797090c131cf85a18b9b4a08d7cbbb2
ZubnayaFeya <danilal@mail.ru>
2021-07-26 13:13:51 +0300

    проблемка со стилями

--- hw5/script.js
+++ hw5/script.js
@@ -1,8 +1,49 @@
 //1. Создать функцию, генерирующую шахматную доску. При этом можно использовать любые html-теги по своему желанию. Доска должна быть разлинована соответствующим образом, 
 //т.е. чередовать черные и белые ячейки. Строки должны нумероваться числами от 1 до 8, столбцы – латинскими буквами A, B, C, D, E, F, G, H.
 let chees = function() {
-    let body = document.getElementById("body");
-    body.createElement("div")
+    let div = document.createElement("div")
+    let table = document.createElement("table")
+
+    let numbers = ['', 1, 2, 3, 4, 5, 6, 7, 8, '']
+    let alphas = ['', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', '']
+
+    for (a of alphas){
+
+        let td = document.createElement("td")
+
+        if (a === ''){
+            td.style.backgroundColor = "gray"
+            td.style.width = "20px"
+            td.style.height = "50px"
+        } else {
+            td.style.width = "50px"
+            td.style.height = "50px"
+        }
+
+        //td.style.width = "50px"
+
+        for (n of numbers.reverse()){
+
+            let tr = document.createElement("tr")
+
+            if (n === ''){
+                tr.style.backgroundColor = "gray"
+                tr.style.height = "20px"
+                tr.style.width = "50px"
+            } else {
+                tr.style.height = "50px"
+                tr.style.width = "50px"
+            }
+            
+            //tr.style.width = "50px"
+            td.appendChild(tr)
+        }
+        table.appendChild(td)
+    }
+
+
+    div.appendChild(table)
+    document.body.appendChild(div)
 }
 
 
@@ -11,4 +52,5 @@ let chees = function() {
 
 
 //3. *Заменить буквы, обозначающие фигуры, картинками.
-Window.onload = chees;
+Window.onload = chees;  // думал что вызывает функцию после загрузки страницы, а по факту не вызывает вовсе.
+chees()
